4e7e7069b383ae262a4b6010ba6feca8e79e344a
ashutosh <ashutosh382singh@gmail.com>
2021-07-28 13:55:11 +0530

    add more sections

--- js/index.js
+++ js/index.js
@@ -1,2 +1,35 @@
-const tegs=document.querySelectorAll("a");
-console.log(tegs);
+// const mybutton = document.getElementById("myBtn");
+
+// // When the user scrolls down 20px from the top of the document,button appears
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+// // When the user clicks on the button, scrolls to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+
+// const btn = document.querySelector(".top");
+
+// window.scroll(function() {
+//   if (window.scrollTop() > 300) {
+//     btn.classList.add('show');
+//   } else {
+//     btn.classList.remove('show');
+//   }
+// });
+
+// btn.addEventListener('click', function(e) {
+//   e.preventDefault();
+// //   $('html, body').animate({scrollTop:0}, '300');
+// //   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// });
